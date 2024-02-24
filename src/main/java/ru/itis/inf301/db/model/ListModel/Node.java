d5aab4c48e71f6369babc4b16a5e91caea288b2b
Patshin Nikita Arkadyevich <cacadada26@gmail.com>
2024-02-24 17:03:40 +0300

    Добавлена реализация интерфейса List с помощью двухсвязного списка. Реализованы не все методы. Требует доработки.

---
+++ src/main/java/ru/itis/inf301/db/model/ListModel/Node.java
@@ -0,0 +1,46 @@
+package ru.itis.inf301.db.model.ListModel;
+
+import java.util.ArrayList;
+
+public class Node<T> {
+    private T value;
+    private Node<T> next;
+    private Node<T> prev;
+
+    public Node() {
+    }
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public Node(T value, Node<T> next, Node<T> prev) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+}
