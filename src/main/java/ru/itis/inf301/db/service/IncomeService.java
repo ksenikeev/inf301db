11fde26ab16feb59b080850df23c5374ace9b655
pavel610 <78311696+pavel610@users.noreply.github.com>
2024-02-24 14:40:51 +0300

    IncomeService kalinin

--- src/main/java/ru/itis/inf301/db/service/IncomeService.java
+++ src/main/java/ru/itis/inf301/db/service/IncomeService.java
@@ -2,18 +2,37 @@ package ru.itis.inf301.db.service;
 
 import ru.itis.inf301.db.model.Income;
 import ru.itis.inf301.db.model.RecordData;
+import ru.itis.inf301.db.repository.IncomeRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface IncomeService {
+public class IncomeService implements IIncomeService {
 
-    Income getIncome();
+    private IncomeRepository repository;
+    private static Income income;
 
-    //Батуллина
-    void addIncome(String date, Float sum, int categoryCode, String description);
+    public IncomeService() {
+        income = new Income();
+    }
 
-    //Абзалов
-    List<RecordData> getIncomeByPeriod(String dateFrom, String DateTo);
-    //Салямов
-    Float getSumIncomeByPeriod(String dateFrom, String DateTo);
+    @Override
+    public Income getIncome() {
+        return null;
+    }
+
+    @Override
+    public void addIncome(String date, Float sum, int categoryCode, String description) {
+
+    }
+
+    @Override
+    public List<RecordData> getIncomeByPeriod(String dateFrom, String DateTo) {
+        return null;
+    }
+
+    @Override
+    public Float getSumIncomeByPeriod(String dateFrom, String DateTo) {
+        return null;
+    }
 }
