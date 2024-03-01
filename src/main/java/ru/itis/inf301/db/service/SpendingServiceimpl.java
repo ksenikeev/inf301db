896878ca9696e50081a6097405a2d04bcf8559eb
Adelina <adelinasafarova84@gmail.com>
2024-03-02 00:02:08 +0300

    getSpendingByPeriod

--- src/main/java/ru/itis/inf301/db/service/SpendingServiceimpl.java
+++ src/main/java/ru/itis/inf301/db/service/SpendingServiceimpl.java
@@ -3,6 +3,9 @@ package ru.itis.inf301.db.service;
 import ru.itis.inf301.db.model.RecordData;
 import ru.itis.inf301.db.model.Spending;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SpendingServiceimpl implements SpendingService{
@@ -18,11 +21,17 @@ public class SpendingServiceimpl implements SpendingService{
 
     @Override
     public List<RecordData> getSpendingByPeriod(String dateFrom, String DateTo) {
-        /*Spending spending<RecordData> = getSpending()<>;
-        for(RecordData rd: spending){
-
-        }*/
-        return null;
+        List<RecordData> spending = new ArrayList<>();
+        LocalDate from = LocalDate.parse(dateFrom);
+        LocalDate to = LocalDate.parse(DateTo);
+        Date from1 = java.sql.Date.valueOf(from);
+        Date to1 = java.sql.Date.valueOf(to);
+        for(RecordData rd: getSpending().getSpending()){
+            if (rd.getDate().compareTo(from1) >= 0 && rd.getDate().compareTo(to1)<=0){
+                spending.add(rd);
+            }
+        }
+        return spending;
     }
 
     @Override
